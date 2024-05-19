53805d155c88b37957d41b2af441fcd12358f2e5
SL91994 <320sl91994@gmail.com>
2024-05-19 23:29:24 +0900

    chore:✨ Changed to use is_dir_or_file function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use chrysalis::input_args::{Cli, Commands};
+use chrysalis::{
+    input_args::{Cli, Commands},
+    utils::validation::is_dir_or_file,
+};
 
 fn main() {
     let cli = Cli::default();
-
     match &cli.command {
         Commands::Sei(val) => {
-            println!("{:?}", val);
+            println!("{:?}", is_dir_or_file(&val.path));
         }
         Commands::Met(val) => {
-            println!("{:?}", val);
+            println!("{:?}", is_dir_or_file(&val.path));
         }
         Commands::Ast(val) => {
-            println!("{:?}", val);
+            println!("{:?}", is_dir_or_file(&val.path));
         }
     };
 }
