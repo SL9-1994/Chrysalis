1c5f717393084b628a5a0caf393d9b3e6474fe06
SL91994 <320sl91994@gmail.com>
2024-05-19 23:27:58 +0900

    add:✨ utils::validation::is_dir_or_file

--- src/utils/validation.rs
+++ src/utils/validation.rs
@@ -1,1 +1,26 @@
-// TODO: 与えられたパス引数がディレクトリ・ファイルを判別するutil function
+use std::path::PathBuf;
+
+/// Enum representing the type of a path.
+#[derive(Debug)]
+pub enum PathType {
+    File,
+    Directory,
+    NotFound,
+}
+
+/// Determines whether a given path is a directory or a file.
+///
+/// # Arguments
+///
+/// * `path` - A reference to a `PathBuf` representing the path to be checked.
+///
+/// # Returns
+///
+/// * `PathType` - The type of the path: `File`, `Directory`, or `NotFound`.
+pub fn is_dir_or_file(path: &PathBuf) -> PathType {
+    match path {
+        path if path.is_dir() => PathType::Directory,
+        path if path.is_file() => PathType::File,
+        _ => PathType::NotFound,
+    }
+}
